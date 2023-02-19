16f69ac310d599fc52b8eca7db099229785763ed
Tanmay Singh <tanmaysingh0311@gmail.com>
2023-02-19 12:23:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/downloadpdf/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.downloadpdf.util
+
+object Constants {
+    const val WRITE_PDF_REQUEST_CODE = 123
+    const val mimeType = "application/pdf"
+    const val pdfDownloadURL ="https://www.africau.edu/images/default/sample.pdf"
+    const val displayName ="assignment_new.pdf"
+}
